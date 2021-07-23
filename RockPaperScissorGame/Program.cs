394b2da59cc2d3164c09d7cd20633a5723b34046
jjspe <jjspe@DESKTOP-L48V0CL>
2021-07-23 13:24:43 -0400

    Add project files.

---
+++ RockPaperScissorGame/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RockPaperScissorGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Ultimate Rock, Paper, Scissors!");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Play the game");
+            Console.WriteLine("2) Calculate your winning percentage");
+            Console.ReadLine();
+        }
+    }
+}
