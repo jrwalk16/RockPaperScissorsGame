65af60123d5e6b7b5f678340e9ab99adb7b6bb31
jeremywalker1216@Gmail.com <Sonics16!>
2021-07-24 11:55:26 -0400

    created new methods

--- RockPaperScissorGame/Program.cs
+++ RockPaperScissorGame/Program.cs
@@ -7,10 +7,28 @@ namespace RockPaperScissorGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Ultimate Rock, Paper, Scissors!");
-            Console.WriteLine("Choose an option:");
             Console.WriteLine("1) Play the game");
             Console.WriteLine("2) Calculate your winning percentage");
+            Console.WriteLine("3) Exit");
+            Console.WriteLine("Enter a number to choose an option: ");
+            string option = Console.ReadLine();
+
+            while (option = 1)
+            {
+            
+            }
+
             Console.ReadLine();
+        }
+
+        static void Game() 
+        {
+            
+        }
+
+        static void WinPercentage() 
+        {
+        
         }
     }
 }
